610765165bd0c58efccb47aed4ad46c7b433630d
zihanhanhan <835937922@qq.com>
2022-03-27 02:28:52 +0800

    day6

---
+++ src/main/java/com/zihan/blog/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.zihan.blog.web;
+
+import com.zihan.blog.NotFoundException;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+//        int i = 9/0;
+
+        String blog = null;
+        if(blog == null) {
+            throw new NotFoundException("博客不存在");
+        }
+        return "index";
+    }
+}
